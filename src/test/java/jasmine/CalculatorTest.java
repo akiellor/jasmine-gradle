75e9196c3d7b5476d5203e0db8767f151bcfd90c
Andrew Kiellor <akiellor@gmail.com>
2013-03-10 22:00:37 -0700

    Birthday!

---
+++ src/test/java/jasmine/CalculatorTest.java
@@ -0,0 +1,9 @@
+package jasmine;
+
+import jasmine.junit.JasmineSuite;
+import jasmine.junit.JasmineTestRunner;
+import org.junit.runner.RunWith;
+
+@RunWith(JasmineTestRunner.class)
+public class CalculatorTest {
+}
